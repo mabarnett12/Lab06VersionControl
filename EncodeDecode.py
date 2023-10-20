d7f85e367bc28c169ff46efcb1bda27f8e21e52a
mabarnett12 <mblatham@ufl.edu>
2023-10-20 19:29:43 -0400

    Added comments to main file.

--- EncodeDecode.py
+++ EncodeDecode.py
@@ -3,6 +3,8 @@
 
 
 def encode(input_str):
+    """Function to encode user input password by adding 3 to each digit of password.
+       Included exception handling for non-integer in user input string."""
     new_str = ""
 
     try:
@@ -14,16 +16,22 @@ def encode(input_str):
     return new_str
 
 
-#add decode function here
+#add decode function here, decode fucntion written, tested, and then removed for partner to add
 
 
 def get_menu_option():
+    """Print user menu and get user menu selection.
+       Included exception handling for invalid menu option selection of non-integer or outside of 1-3."""
     print('Menu')
     print('-' * 13)
     print('1. Encode')
     print('2. Decode')
     print('3. Quit\n')
-    user_option = int(input('Please enter an option: '))
+
+    try:
+        user_option = int(input('Please enter an option: '))
+    except:
+        raise ValueError("Invalid Menu Selection")
 
     if not (1 <= user_option <= 3):
        raise ValueError("Invalid Menu Selection")
@@ -39,12 +47,17 @@ def main():
         while menu_option != 3:
 
             if menu_option == 1:
+                #get and encode user input password
                 input_pw = input('Please enter your password to encode: ')
                 encoded_pw = encode(input_pw)
                 print('Your password has been encoded and stored!\n')
 
 
             elif menu_option == 2:
+                #decode encoded password back to original user entry and print both
+                #exception handling included if user has not yet input a password to encode
+                #but selects menu option 2 to decode
+
                 if input_pw == " ":
                     raise ValueError('Password to encode not yet entered.')
                 decoded_pw = decode(encoded_pw)
@@ -52,6 +65,7 @@ def main():
 
             menu_option = get_menu_option()
 
+    #exception handling for various invald user entries in both functions and main
     except ValueError as excpt:
         print(excpt)
         print('Unable to complete request. Please check input and rerun program.')
